b8e75fcb72de3791e0123f3f807b4014c5da64ff
Alexander Buiko <reed665@hotmail.com>
2018-05-30 10:57:04 +0300

    add flashcard button

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,12 @@
+import card from '@/core/card'
+
 export default {
   removeCard (state, { idx, card }) {
     state.cards = state.cards.filter((card, index) => index !== idx)
+  },
+
+  addCard (state, cardData) {
+    const cardToAdd = card(cardData)
+    state.cards.unshift(cardToAdd)
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h2 class="title">Flashcards</h2>
     <!-- <img src="../assets/logo.png"> -->
+    <button class="btn-add" @click="addCard">Add Flashcard</button>
     <card-list :data="cards" />
   </div>
 </template>
@@ -16,7 +17,12 @@ export default {
     cards () {
       return this.$store.state.cards
     }
-  }
+  },
+  methods: {
+    addCard () {
+      this.$store.commit('addCard')
+    }
+  },
 }
 </script>
 
@@ -31,6 +37,11 @@ export default {
     margin: 0;
     padding: 20px;
     border-bottom: 1px solid black;
+  }
+
+  .btn-add {
+    padding: 4px;
+    margin: 10px 20px;
   }
 
   @media (min-width: 1080px) {
